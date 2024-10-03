1db3a330050d1536591c7c4206bfcb586da232c4
Zreun <43525339+Zreun@users.noreply.github.com>
2024-10-03 22:53:48 +0200

    Update question3.cpp

--- chapter13/question3.cpp
+++ chapter13/question3.cpp
@@ -1,8 +1,8 @@
 /*
-Define an enum class named Animal that contains the following animals: pig, chicken, goat, cat, dog, duck. 
+Define an enum class named Animal that contains the following animals: pig, chicken, goat, cat, dog, duck.
 Write a function named getAnimalName() that takes an Animal parameter and uses a switch statement to return the name for that animal
-as a std::string_view (or std::string if you’re using C++14). Write another function named printNumberOfLegs() 
-that uses a switch statement to print the number of legs each animal walks on. Make sure both functions have a default case that prints an error message. 
+as a std::string_view (or std::string if you’re using C++14). Write another function named printNumberOfLegs()
+that uses a switch statement to print the number of legs each animal walks on. Make sure both functions have a default case that prints an error message.
 Call printNumberOfLegs() from main() with a cat and a chicken. Your output should look like this:
 */
 
@@ -30,25 +30,45 @@ constexpr std::string_view getAnimal(Animal animal)
 
     switch (animal)
     {
-    case pig: return "2";
-    case chicken: return "2";
-    case goat: return "4";
-    case cat: return "4";
-    case dog: return "4";
-    case duck; return "4";
+    case pig: return "pig";
+    case chicken: return "chicken";
+    case goat: return "goat";
+    case cat: return "cat";
+    case dog: return "dog";
+    case duck: return "duck";
 
-    default: return "???";
+        default: return "???";
 
     }
 }
 
+void printNumberOflegs(Animal animal)
+{
 
+    using enum Animal;
 
-int main(){
+    std::cout << "A " << getAnimal(animal) << " has ";
 
-    Animal nbpatte{ Animal::cat };
+    switch (animal)
+    {
+    case duck:;
+    case chicken: ;
+        std::cout << "2 \n";
+        break;
+    case goat: ;
+    case pig:;
+    case cat: ;
+    case dog: ;
+        std::cout << "4 \n";
+        break;
+    }
+}
 
-    std::cout << "l'animal à " << getAnimal(nbpatte) << "\n";
+int main() {
+
+    printNumberOflegs(Animal::cat);
+    printNumberOflegs(Animal::chicken);
+
 
     return 0;
 }
